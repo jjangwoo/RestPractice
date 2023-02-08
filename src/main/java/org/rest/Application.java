021657fd3abcd0a5a9566c680b926fcfb82892f2
jjangwoo <mongdang88@naver.com>
2023-02-08 14:16:59 +0900

    init

--- src/main/java/org/rest/Application.java
+++ src/main/java/org/rest/Application.java
@@ -2,9 +2,8 @@ package org.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+//@EnableFeignClients
 @SpringBootApplication
 public class Application {
   public static void main(String[] args){
--- src/main/java/org/rest/infrastructure/ext/feign/NaverFeignClient.java
+++
@@ -1,8 +0,0 @@
-package org.rest.infrastructure.ext.feign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "NaverFeignClient", url = "${}")
-public interface NaverFeignClient {
-
-}
